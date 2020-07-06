f3977a8e26c7b7f57b49acfe58e441eca8fd4de7
Lucy L <lucy.liu2333@gmail.com>
2020-07-06 10:03:37 -0400

    added updating markerclusters

--- static/markers.js
+++ static/markers.js
@@ -12,23 +12,49 @@ if (feature.properties){
   }
 }
 
+var geojsonMarkerOptions = {
+  radius: 8,
+  fillColor: "#ff7800",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8
+};
+
+function pointToLayer(feature,latlng){
+  var marker = L.circleMarker(latlng,geojsonMarkerOptions);
+  //marker.bindPopup(feature.properties.Location + '<br/>' + feature.properties.OPEN_DT);
+  return marker;
+}
+var mcg = L.markerClusterGroup();
+
 function getJson(){
   $.getJSON(url, function(data){
       if(data.length < 15){
         lastData.append(data);
         data = lastData;
       }
+      //creates new layers of markers to keep refreshing 
       //map.removeLayer(geoJsonLayer);
-      //geoJsonLayer = L.geoJson(data, {onEachFeature: popUp});
-      //geoJsonLayer.addTo(map);
+      //mcg.clearLayers()
+      geoJsonLayer = L.geoJson(data, {onEachFeature: popUp});
+      mcg.addLayer(geoJsonLayer)
+      map.addLayer(mcg);
   });
 }
 
+//var clusters = L.markerClusterGroup();
 (function(){
   $.getJSON(url, function(data){
     geoJsonLayer = L.geoJson(data, {onEachFeature: popUp});
     lastData = data;
-    geoJsonLayer.addTo(map);
+    //geoJsonLayer.addTo(map);
+    //var newLayer = L.featureGroup.subGroup(mcg);
+
+    geoJsonLayer = L.geoJson(data, {onEachFeature: popUp});
+    mcg.addLayer(geoJsonLayer)
+    //newLayer.addLayer(geoJsonLayer)
+    map.addLayer(mcg);
   });
-  var counter = setInterval(getJson, 5000);
+var counter = setInterval(getJson, 5000);
 })();
